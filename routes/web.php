af927c8627654bd3f45b1d62450e8f94cc086591
Lucian Iernye <lucian.iernye@gmail.com>
2022-04-06 22:36:55 +0100

    add register controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/register', 'Auth\RegisterController@index')->name('register'); - old way
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+
 Route::get('/posts', function () {
- return view('posts.index');
+    return view('posts.index');
 });
