c1ee0a3bd184d369b919e8e5b6d01af226aa909b
Lucian Iernye <lucian.iernye@gmail.com>
2022-04-06 20:42:30 +0100

    add tailwind to the project and add navbar

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-some content
+<div class="flex justify-center">
+    <div class="w-8/12 bg-white p-6 rounded">
+        <h1>Posts</h1>
+    </div>
+</div>
 @endsection
